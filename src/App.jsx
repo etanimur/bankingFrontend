cdecdb3ebda69b5c4afc709bde52264dbff0557e
Raghav Aloria <raghavaloria@gmail.com>
2025-06-12 23:35:52 +0530

    folder strucutre for different features - login , signup , transfer , transactions

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/dashboard/dashboard";
+import TransferForm from "./pages/dashboard/transfer/TransferForm";
+import Login from "./pages/Authentication/login/login";
+import Signup from "./pages/Authentication/signup/signup";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/statistics" element={<Dashboard />} />
-        <Route path="/cards" element={<Dashboard />} />
-        <Route path="/banking" element={<Dashboard />} />
-        <Route path="/settings" element={<Dashboard />} />
-        <Route path="/profile" element={<Dashboard />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/transfer" element={<TransferForm />} />
+        <Route path="/profile" element={<Login />} />
       </Routes>
     </Router>
   );
